a9b2e824620877170270a1255e02977cfcaf837e
st-canpham <can.pham@supremetech.vn>
2023-04-16 20:19:03 +0700

    fix exercise 1

--- src/app/core/helpers/checkArrayAscending.test.ts
+++ src/app/core/helpers/checkArrayAscending.test.ts
@@ -1,41 +1,105 @@
 import { checkArrayAscending } from './checkArrayAscending';
 
-test('Test case 1', () => {
-  expect(checkArrayAscending([])).toBeFalsy();
+describe('Đối số không phải là Array', () => {
+  it ('Đối số là null', () => {
+    expect(checkArrayAscending(null)).toBe(false);
+  });
+
+  it ('Đối số là undefined', () => {
+    expect(checkArrayAscending(undefined)).toBe(false);
+  });
+  
+  it ('Đối số là NaN', () => {
+    expect(checkArrayAscending(NaN)).toBe(false);
+  });
+
+  it ('Đối số là number', () => {
+    expect(checkArrayAscending(1)).toBe(false);
+  });
+
+  it ('Đối số là string', () => {
+    expect(checkArrayAscending('[1, 2, 3]')).toBe(false);
+  });
+
+  it ('Đối số là function', () => {
+    expect(checkArrayAscending(() => {})).toBe(false);
+  });
+
+  it ('Đối số là object', () => {
+    expect(checkArrayAscending({ a: 1 })).toBe(false);
+  });
 });
 
-test('Test case 2', () => {
-  expect(checkArrayAscending([1])).toBeFalsy();
+describe('Đối số là mảng không phải là mảng number', () => {
+  it ('Đối số là mảng rỗng', () => {
+    expect(checkArrayAscending([])).toBe(false);
+  });
+
+  it ('Đối số là mảng string', () => {
+    expect(checkArrayAscending([ '3', '4', '5' ])).toBe(false);
+  });
+
+  it ('Đối số là mảng vừa number vừa string', () => {
+    expect(checkArrayAscending([ 1, 2, '3', '4', '5' ])).toBe(false);
+  });
+
+  it ('Đối số là mảng lồng nhau', () => {
+    expect(checkArrayAscending([ 1, 3, 5, [ 6, 7 ] ])).toBe(false);
+  });
+
+  it ('Đối số là mảng có object', () => {
+    expect(checkArrayAscending([ 1, 3, 5, { a: 7 } ])).toBe(false);
+  });
+
+  it ('Đối số là mảng có function', () => {
+    expect(checkArrayAscending([ 1, 3, 5, () => {} ])).toBe(false);
+  });
+
+  it ('Đối số là mảng có null', () => {
+    expect(checkArrayAscending([ 1, 3, 5, 7, null ])).toBe(false);
+  });
+
+  it ('Đối số là mảng có undefined', () => {
+    expect(checkArrayAscending([ 1, 3, 5, 7, undefined ])).toBe(false);
+  });
+
+  it ('Đối số là mảng có NaN', () => {
+    expect(checkArrayAscending([ 1, 3, 5, 7, NaN ])).toBe(false);
+  });
 });
 
-test('Test case 3', () => {
-  expect(checkArrayAscending([1, 22, 4, 6])).toBeFalsy();
+describe('Đối số là mảng số không hợp lệ', () => {
+  it ('Đối số là mảng number có 1 phần tử', () => {
+    expect(checkArrayAscending([1])).toBe(false);
+  });
+
+  it ('Đối số là mảng number không tăng dần', () => {
+    expect(checkArrayAscending([ 2, 1, 4, 5, 3, 8 ])).toBe(false);
+  });
+
+  it ('Đối số là mảng số thực không tăng dần', () => {
+    expect(checkArrayAscending([ 2, 1.5, 4, 5, 3.2, 8 ])).toBe(false);
+  });
+
+  it ('Đối số là mảng có số âm không tăng dần', () => {
+    expect(checkArrayAscending([ 2, 3, 5, 7, -10 ])).toBe(false);
+  });
 });
 
-test('Test case 4', () => {
-  expect(checkArrayAscending([1, 22, 4.4, 6])).toBeFalsy();
-});
+describe('Đối số là mảng hợp lệ', () => {
+  it ('Đối số là mảng số có 2 số bằng nhau', () => {
+    expect(checkArrayAscending([ 1, 1 ])).toBe(true);
+  });
 
-test('Test case 5', () => {
-  expect(checkArrayAscending(null)).toBeFalsy();
-});
+  it ('Đối số là mảng số có số thực tăng dần', () => {
+    expect(checkArrayAscending([ 2, 3.3, 5, 7.5, 8, 10 ])).toBe(true);
+  });
 
-test('Test case 6', () => {
-  expect(checkArrayAscending(undefined)).toBeFalsy();
-});
+  it ('Đối số là mảng số có số âm tăng dần', () => {
+    expect(checkArrayAscending([ -10, 3, 5, 7, 8, 10 ])).toBe(true);
+  });
 
-test('Test case 7', () => {
-  expect(checkArrayAscending([ 2, 3, 5, 7, null ])).toBeFalsy();
-});
-
-test('Test case 8', () => {
-  expect(checkArrayAscending([ 2, 3, 5, 7, undefined ])).toBeFalsy();
-});
-
-test('Test case 9', () => {
-  expect(checkArrayAscending([ 2, 3, 5, 7, 8, 10 ])).toBeTruthy();
-});
-
-test('Test case 10', () => {
-  expect(checkArrayAscending([ 2, 3.4, 5, 7.2, 8, 10 ])).toBeTruthy();
+  it ('Đối số là mảng số tăng dần', () => {
+    expect(checkArrayAscending([ 2, 3, 5, 7, 8, 10 ])).toBe(true);
+  });
 });
--- src/app/core/helpers/checkArrayAscending.ts
+++ src/app/core/helpers/checkArrayAscending.ts
@@ -2,15 +2,17 @@ export const onlyNumbers = (array): boolean => {
   if (!Array.isArray(array)) {
     return false;
   }
-  return array.every(element => {
-    return typeof element === 'number';
+  return array.every((element) => {
+    return element && typeof element === 'number';
   });
 };
 
-export const checkArrayAscending = (array: number[]): boolean => {
+export const checkArrayAscending = (array: any): boolean => {
   if (!onlyNumbers(array) || array.length < 2) {
     return false;
   }
   const newArray = [...array];
-  return JSON.stringify(newArray.sort((a, b) => a - b)) === JSON.stringify(array);
+  return (
+    JSON.stringify(newArray.sort((a, b) => a - b)) === JSON.stringify(array)
+  );
 };
