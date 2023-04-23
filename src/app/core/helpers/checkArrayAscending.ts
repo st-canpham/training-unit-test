2562a2d8327e5e62f4cbb9e01bbd95ec166622ba
st-canpham <can.pham@supremetech.vn>
2023-04-24 01:59:59 +0700

    update exercise 1

--- src/app/core/helpers/checkArrayAscending.ts
+++ src/app/core/helpers/checkArrayAscending.ts
@@ -1,18 +1,8 @@
-export const onlyNumbers = (array): boolean => {
-  if (!Array.isArray(array)) {
-    return false;
-  }
-  return array.every((element) => {
-    return element && typeof element === 'number';
-  });
-};
-
 export const checkArrayAscending = (array: any): boolean => {
-  if (!onlyNumbers(array) || array.length < 2) {
+  if (!Array.isArray(array) || array.length < 2) {
     return false;
   }
-  const newArray = [...array];
-  return (
-    JSON.stringify(newArray.sort((a, b) => a - b)) === JSON.stringify(array)
-  );
+  return array.every((item: number, index: number) => {
+    return typeof item === 'number' && (index === 0 || item >= array[index - 1]);
+  });
 };
